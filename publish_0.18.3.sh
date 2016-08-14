f68e38f918e40794bd55791bb2896988be646efc
ayatakesi <ayanokoji.takesi@gmail.com>
2016-08-15 07:01:38 +0900

    html/

--- publish_0.18.3.sh
+++ publish_0.18.3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # 開発用リポジトリから公開用リポジトリへドキュメントをコピーする
-cp -p html/*.html *.html *.info *.pdf *.tar.gz ../ayatakesi.github.io/gettext/0.18.3/
+cp -pR html/ *.html *.info *.pdf *.tar.gz ../ayatakesi.github.io/gettext/0.18.3/
